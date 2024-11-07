0ffe087a9547d7bdea16d74feb1a5913326d9992
Ali Yara <ali.yara.fr@gmail.com>
2024-11-07 14:27:45 +0100

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",    // Adjust paths based on your project structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
